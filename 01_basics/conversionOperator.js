9d0aa269badc59a38a62c573bd94e61626288b5a
amanshukl <amanshuk916@gmail.com>
2025-12-21 17:17:58 +0530

    Comment out conversion operator examples for clarity

--- 01_basics/conversionOperator.js
+++ 01_basics/conversionOperator.js
@@ -1,10 +1,17 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);//Number
-console.log(valueInNumber);//NaN(not a number)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//Number
+// console.log(valueInNumber);//NaN(not a number)
 
 
+//*****************Operations********************* */
+
+// 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
